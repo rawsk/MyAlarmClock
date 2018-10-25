60c6d23dbbf13ad35140b9c6251e5415e4170281
rawsk <raws.0705.k@gmail.com>
2018-10-25 18:26:49 +0900

    ankoを用いて簡略化

--- app/src/main/java/sample/myalarmclock/AlarmBroadcastReceiver.kt
+++ app/src/main/java/sample/myalarmclock/AlarmBroadcastReceiver.kt
@@ -3,14 +3,15 @@ package sample.myalarmclock
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
 
 class AlarmBroadcastReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val intent = Intent(context, MainActivity::class.java)
-            .putExtra("onReceive", true)
-            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context?.startActivity(intent)
+        context?.run {
+            startActivity(
+                intentFor<MainActivity>("onReceive" to true).newTask())
+        }
     }
 }
